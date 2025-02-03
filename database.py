73861e6d1c70aa285c61c3838d565e4e8fc8d19f
archoff <vipusk123@gmail.com>
2025-02-03 14:24:58 +0700

    added func for session in database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 DATABASE_URL = "sqlite:///database.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
@@ -8,3 +8,9 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
